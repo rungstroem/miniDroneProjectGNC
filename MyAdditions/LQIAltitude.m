95cd72a4e74daca0c62ec60fb215917a60809bc4
Kenneth <rungstroem@hotmail.com>
2021-05-08 13:56:40 +0200

    Started on LQI

--- MyAdditions/LQIAltitude.m
+++ MyAdditions/LQIAltitude.m
@@ -3,3 +3,23 @@
 g = 9.82;
 m = 0.063; %grams
 
+% Create system state space model
+A = [0 1; 0 0];
+B = [(1/m) (1/m)]';
+C = [1 0];
+D = 0;
+
+% Create system from state space matrices
+sys = ss(A,B,C,D);
+
+% Define Q and R tuning matrices
+Q = [   1/10^2 0 0;
+        0 1/12^2 0;
+        0 0 ];
+    % Q_ii = maximum acceptable value of x_i^2
+% For this size system R can only be of dimensions 1x1 -scalar
+R = 1/(4*Controller.totalThrustMaxRelative*Controller.motorsThrustPerMotorMax)^2;
+    % R_ii = maximum acceptable value of u_i^2
+
+% Calculate LQI gain
+[KLQI, S, CLP] = lqi(sys, Q, R, 0);
